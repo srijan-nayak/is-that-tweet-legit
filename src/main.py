a8a0b67bcfa3a447fde34a3839e4c43ca8be8ce1
Srijan Nayak <srijannayak0@gmail.com>
2022-11-19 13:05:17 +0530

    feat: Load API credentials from .env file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
+from dotenv import load_dotenv
+from os import environ as env
+
+load_dotenv()
+
+API_KEY = env["API_KEY"]
+API_KEY_SECRET = env["API_KEY_SECRET"]
+BEARER_TOKEN = env["BEARER_TOKEN"]
+
 st.write("# Is that Tweet legit?")
