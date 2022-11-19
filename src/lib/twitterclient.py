6715dcf4eba9dcab0dbc55b4cb8a685e9a1d7f26
Srijan Nayak <srijannayak0@gmail.com>
2022-11-19 20:35:52 +0530

    feat: Create TwitterClient.get_tweet_details method

--- src/lib/twitterclient.py
+++ src/lib/twitterclient.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class TwitterClient:
     __API_ROOT = "https://api.twitter.com/2"
     __TWEETS_ENDPOINT = f"{__API_ROOT}/tweets"
@@ -5,3 +8,9 @@ class TwitterClient:
 
     def __init__(self, bearer_token: str):
         self.__headers = {"Authorization": f"Bearer {bearer_token}"}
+
+    def get_tweet_details(self, tweet_id: str) -> dict:
+        tweet_fields = ",".join(["author_id"])
+        return requests.get(f"{self.__TWEETS_ENDPOINT}/{tweet_id}",
+                            params={"tweet.fields": tweet_fields},
+                            headers=self.__headers).json()["data"]
