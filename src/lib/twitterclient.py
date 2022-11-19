58031c3f52a47ea37fc74b5ee17f1fcfe0cf9428
Srijan Nayak <srijannayak0@gmail.com>
2022-11-19 20:33:11 +0530

    feat: Specify API endpoints in TwitterClient class

--- src/lib/twitterclient.py
+++ src/lib/twitterclient.py
@@ -1,3 +1,7 @@
 class TwitterClient:
+    __API_ROOT = "https://api.twitter.com/2"
+    __TWEETS_ENDPOINT = f"{__API_ROOT}/tweets"
+    __USERS_ENDPOINT = f"{__API_ROOT}/users"
+
     def __init__(self, bearer_token: str):
         self.__headers = {"Authorization": f"Bearer {bearer_token}"}
