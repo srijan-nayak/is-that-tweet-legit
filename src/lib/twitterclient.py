ea6d3a32bb025168f3830ef57ad5a601811c515b
Srijan Nayak <srijannayak0@gmail.com>
2022-11-22 10:49:29 +0530

    style: Format code with Black formatter

--- src/lib/twitterclient.py
+++ src/lib/twitterclient.py
@@ -9,14 +9,22 @@ class TwitterClient:
     def __init__(self, bearer_token: str):
         self.__headers = {"Authorization": f"Bearer {bearer_token}"}
 
-    def fetch_tweet_details(self, tweet_id: str, additional_fields: list[str] = None) -> dict:
+    def fetch_tweet_details(
+        self, tweet_id: str, additional_fields: list[str] = None
+    ) -> dict:
         tweet_fields = ",".join(additional_fields) if additional_fields else []
-        return requests.get(f"{self.__TWEETS_ENDPOINT}/{tweet_id}",
-                            params={"tweet.fields": tweet_fields},
-                            headers=self.__headers).json()
+        return requests.get(
+            f"{self.__TWEETS_ENDPOINT}/{tweet_id}",
+            params={"tweet.fields": tweet_fields},
+            headers=self.__headers,
+        ).json()
 
-    def fetch_user_details(self, user_id: str, additional_fields: list[str] = None) -> dict:
+    def fetch_user_details(
+        self, user_id: str, additional_fields: list[str] = None
+    ) -> dict:
         user_fields = ",".join(additional_fields) if additional_fields else []
-        return requests.get(f"{self.__USERS_ENDPOINT}/{user_id}",
-                            params={"user.fields": user_fields},
-                            headers=self.__headers).json()
+        return requests.get(
+            f"{self.__USERS_ENDPOINT}/{user_id}",
+            params={"user.fields": user_fields},
+            headers=self.__headers,
+        ).json()
--- src/main.py
+++ src/main.py
@@ -11,11 +11,15 @@ def get_all_details(tweet_url_or_id: str, twitter_client: TwitterClient) -> dict
     all_details = {}
 
     tweet_id = tweet_url_or_id.split("/")[-1]
-    tweet_details_response = twitter_client.fetch_tweet_details(tweet_id, ["public_metrics", "author_id"])
+    tweet_details_response = twitter_client.fetch_tweet_details(
+        tweet_id, ["public_metrics", "author_id"]
+    )
     all_details["tweet"] = tweet_details_response["data"]
 
     user_id = all_details["tweet"]["author_id"]
-    user_details_response = twitter_client.fetch_user_details(user_id, ["public_metrics"])
+    user_details_response = twitter_client.fetch_user_details(
+        user_id, ["public_metrics"]
+    )
     all_details["user"] = user_details_response["data"]
 
     return all_details
@@ -24,7 +28,7 @@ def get_all_details(tweet_url_or_id: str, twitter_client: TwitterClient) -> dict
 load_dotenv()
 BEARER_TOKEN = env["BEARER_TOKEN"]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     st.write("# Is that Tweet legit?")
 
     twitter_client = TwitterClient(BEARER_TOKEN)
