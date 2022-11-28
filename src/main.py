36c63ee1e93a9f0477db2aaadc396819af2d9bbb
Srivika Srinivas <nsrivika@gmail.com>
2022-11-28 17:32:26 +0530

    feat:make srivika change

--- src/main.py
+++ src/main.py
@@ -50,6 +50,7 @@ if __name__ == "__main__":
     load_dotenv()
     twitter_client = TwitterClient(env["BEARER_TOKEN"])
 
+
     # RAKSHA
 
     # PRADEEP
@@ -57,3 +58,4 @@ if __name__ == "__main__":
     # SRINIVAS
 
     # SRIVIKA
+    "srivika's change"
